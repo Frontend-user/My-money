9c2f128db46220268b0346da78fe9d23872d73f0
Robert <artemyoutubed@mail.ru>
2023-03-10 16:44:32 +0300

    del add category del add expense

---
+++ Api/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class CategoryForm(forms.Form):
+    text = forms.CharField(max_length=200)
+
+class ExpenseForm(forms.Form):
+    expense_text = forms.CharField(max_length=400)
